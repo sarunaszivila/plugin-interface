feb5277f2d158fe802a104df9bd1d7b7c9f0f97d
plentymonkey <10546500+plentymonkey@users.noreply.github.com>
2021-04-21 12:02:23 +0200

    Automated interface update 2021-04-21_12:02:23

---
+++ Modules/Webshop/Template/Contracts/AssetRepositoryContract.php
@@ -0,0 +1,20 @@
+<?php
+namespace Plenty\Modules\Webshop\Template\Contracts;
+
+
+/**
+ * Register required assets for the frontend.
+ */
+interface AssetRepositoryContract 
+{
+
+	/**
+	 * Add a path to an asset that should be pushed to the client using HTTP2 server push
+	 */
+	public function addAsset(
+		string $path, 
+		string $as = "", 
+		string $rel = "preload"
+	);
+
+}
