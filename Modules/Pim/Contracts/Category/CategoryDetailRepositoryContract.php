044b36004916f3b125ed25bc26ac26273be07cc3
plentymonkey <10546500+plentymonkey@users.noreply.github.com>
2021-04-23 00:02:34 +0200

    Automated interface update 2021-04-23_00:02:34

---
+++ Modules/Pim/Contracts/Category/CategoryDetailRepositoryContract.php
@@ -0,0 +1,67 @@
+<?php
+namespace Plenty\Modules\Pim\Contracts\Category;
+
+use Exception;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Support\Collection;
+use Plenty\Modules\Category\Models\CategoryDetails;
+use Plenty\Repositories\Contracts\FilterableContract;
+use Plenty\Repositories\Criteria\Contracts\CriteriableContract;
+use Plenty\Repositories\Criteria\Criteria;
+
+/**
+ * To be written...
+ */
+interface CategoryDetailRepositoryContract 
+{
+
+	/**
+	 * Search category details
+	 */
+	public function search(
+		array $with = [], 
+		int $page = 1, 
+		int $perPage = 50
+	):array;
+
+	public function getCount(
+	):int;
+
+	/**
+	 * Resets all Criteria filters by creating a new instance of the builder object.
+	 */
+	public function clearCriteria(
+	);
+
+	/**
+	 * Applies criteria classes to the current repository.
+	 */
+	public function applyCriteriaFromFilters(
+	);
+
+	/**
+	 * Sets the filter array.
+	 */
+	public function setFilters(
+		array $filters = []
+	);
+
+	/**
+	 * Returns the filter array.
+	 */
+	public function getFilters(
+	);
+
+	/**
+	 * Returns a collection of parsed filters as Condition object
+	 */
+	public function getConditions(
+	);
+
+	/**
+	 * Clears the filter array.
+	 */
+	public function clearFilters(
+	);
+
+}
