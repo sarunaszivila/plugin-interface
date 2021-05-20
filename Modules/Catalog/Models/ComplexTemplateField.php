9b2fbf363823782134bf9ff76534d3d352ddc943
plentymonkey <10546500+plentymonkey@users.noreply.github.com>
2021-05-21 00:05:02 +0200

    Automated interface update 2021-05-21_00:05:02

--- Modules/Catalog/Models/ComplexTemplateField.php
+++ Modules/Catalog/Models/ComplexTemplateField.php
@@ -1,20 +1,73 @@
 <?php
 namespace Plenty\Modules\Catalog\Models;
 
+use Plenty\Modules\Catalog\Callables\EmptyCatalogTemplateFieldCallable;
+use Plenty\Modules\Catalog\Constants\CatalogMappingTypes;
+use Plenty\Modules\Catalog\Containers\CatalogTemplateFieldContainer;
+use Plenty\Modules\Catalog\Contracts\CatalogMappingValueProviderContract;
+use Plenty\Modules\Catalog\Contracts\CatalogTemplateFieldCallableContract;
+use Plenty\Modules\Catalog\Contracts\CatalogTemplateFieldContract;
 
 /**
  * A complex template field is used for Mappings with predefined valid values. The user has to choose a valid value and then provide a condition, which determines in which cases this value will be sent.
  */
-abstract class ComplexTemplateField 
+abstract class ComplexTemplateField implements \Plenty\Modules\Catalog\Contracts\CatalogTemplateFieldContract
+
 {
 
 	/**
-	 * Returns this model as an array.
+	 * ComplexTemplateField constructor.
 	 */
-	public function toArray(
-	):array
-	{
-		return [];
-	}
+	abstract public function __construct(
+		string $exportKey, 
+		string $key, 
+		string $label, 
+		CatalogMappingValueProviderContract $catalogMappingValueProvider, 
+		bool $isRequired = false, 
+		bool $isLocked = false, 
+		bool $isArray = false, 
+		array $meta = [], 
+		array $defaultSources = []
+	);
+
+	abstract public function getKey(
+	):string;
+
+	abstract public function getLabel(
+	):string;
+
+	abstract public function isRequired(
+	):bool;
+
+	abstract public function isLocked(
+	):bool;
+
+	abstract public function isArray(
+	):bool;
+
+	abstract public function getMeta(
+	):array;
+
+	abstract public function getCallable(
+	):CatalogTemplateFieldCallableContract;
+
+	abstract public function getMappingValueProvider(
+	):CatalogMappingValueProviderContract;
+
+	abstract public function getDefaultSources(
+	):array;
+
+	abstract public function getExportKey(
+	):string;
+
+	abstract public function getNestedFields(
+	):CatalogTemplateFieldContainer;
+
+	abstract public function getType(
+	):int;
+
+	abstract public function setCallable(
+		CatalogTemplateFieldCallableContract $callable
+	);
 
 }
