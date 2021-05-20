e32ef1c8f887e9ccb008404c1784d2be79fbf2d7
plentymonkey <10546500+plentymonkey@users.noreply.github.com>
2021-05-21 00:03:46 +0200

    Automated interface update 2021-05-21_00:03:46

--- Modules/Catalog/Models/CombinedTemplateField.php
+++ Modules/Catalog/Models/CombinedTemplateField.php
@@ -11,8 +11,25 @@ use Plenty\Modules\Catalog\Contracts\CatalogTemplateFieldContract;
 /**
  * A combined template field is a mix out of simple and complex template fields. You can define a field with predefined valid values, which will be exported if all nested fields were filled successfully in the export. An example is a barcode type. The customer can choose a barcode type, that is exported if the field barcode was successfully mapped with a specific source.
  */
-abstract class CombinedTemplateField 
+abstract class CombinedTemplateField implements \Plenty\Modules\Catalog\Contracts\CatalogTemplateFieldContract
+
 {
+
+	/**
+	 * CombinedTemplateField constructor.
+	 */
+	abstract public function __construct(
+		string $exportKey, 
+		string $key, 
+		string $label, 
+		CatalogMappingValueProviderContract $catalogMappingValueProvider, 
+		CatalogTemplateFieldContainer $catalogTemplateFieldContainer, 
+		bool $isRequired = false, 
+		bool $isLocked = false, 
+		bool $isArray = false, 
+		array $meta = [], 
+		array $defaultSources = []
+	);
 
 	abstract public function getKey(
 	):string;
