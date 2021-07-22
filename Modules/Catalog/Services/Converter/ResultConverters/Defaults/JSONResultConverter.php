8ee795b61427a0419fbc6a626ae77e31bfb75f41
plentymonkey <10546500+plentymonkey@users.noreply.github.com>
2021-07-23 00:02:45 +0200

    Automated interface update 2021-07-23_00:02:45

--- Modules/Catalog/Services/Converter/ResultConverters/Defaults/JSONResultConverter.php
+++ Modules/Catalog/Services/Converter/ResultConverters/Defaults/JSONResultConverter.php
@@ -12,7 +12,8 @@ use Plenty\Modules\Catalog\Services\FileHandlers\ResourceHandler;
 /**
  * Default JSON Result converter
  */
-abstract class JSONResultConverter 
+abstract class JSONResultConverter extends \Plenty\Modules\Catalog\Services\Converter\ResultConverters\BaseResultConverter 
+
 {
 
 	const CHUNK_SIZE = 50;
@@ -21,69 +22,107 @@ abstract class JSONResultConverter
 
 	const FILE_EXTENSION = 'json';
 
+	abstract public function getKey(
+	):string;
+
+	abstract public function getLabel(
+	):string;
+
+	/**
+	 * Convert and prepare resource for download
+	 */
 	abstract public function toDownload(
-		 $resourceHandler
+		ResourceHandler $resourceHandler
 	);
 
+	/**
+	 * Convert and prepare data for marketplace
+	 */
 	abstract public function toMarketplace(
 	);
 
 	/**
-	 * When you don't know what you have but you know what you want
-You give it almost anything and you hope
-Hope dies last
+	 * Convert from an array
 	 */
-	abstract public function from(
-		 $data
-	):BaseResultConverter;
-
 	abstract public function fromArray(
-		 $data
-	);
+		array $data
+	):CatalogResultConverterContract;
 
+	/**
+	 * Convert from a Collection
+	 */
 	abstract public function fromCollection(
-		 $collection
-	);
+		Collection $collection
+	):CatalogResultConverterContract;
 
+	/**
+	 * Convert from a CatalogLazyCollection
+	 */
 	abstract public function fromCatalogLazyCollection(
-		 $catalogLazyCollection
-	);
+		CatalogLazyCollection $catalogLazyCollection
+	):CatalogResultConverterContract;
 
+	/**
+	 * Convert from a CatalogExportResult
+	 */
 	abstract public function fromCatalogExportResult(
-		 $exportResult
-	);
+		CatalogExportResult $exportResult
+	):CatalogResultConverterContract;
 
-	abstract public function fromRawJSONLFile(
-		 $sourceFile
-	);
-
+	/**
+	 * Get the lazy collection's chunk size
+	 */
 	abstract public function getChunkSize(
-	);
+	):int;
 
+	/**
+	 * Get the mime type of the resource returned by this converter
+	 */
 	abstract public function getMIME(
-	);
+	):string;
 
+	/**
+	 * Get the file extension of the resource returned by this converter
+	 */
 	abstract public function getFileExtension(
-	);
+	):string;
 
+	/**
+	 * Get the source lazy collection
+	 */
 	abstract public function getSourceCollection(
-	);
+	):CatalogLazyCollection;
 
+	/**
+	 * Set settings that you need to use in your converter
+	 */
 	abstract public function setSettings(
-		 $settings = []
-	);
+		array $settings = []
+	):CatalogResultConverterContract;
 
+	/**
+	 * Through this a single entry of a catalog result should be added
+	 */
 	abstract public function addData(
 		 $data
 	);
 
+	/**
+	 * Converts the data that was inserted into the specific output format
+	 */
 	abstract public function getConvertedResult(
-	);
+	):string;
 
+	/**
+	 * Removes all the data that was previously added to this instance
+	 */
 	abstract public function clear(
 	);
 
+	/**
+	 * Returns the type of the converted result
+	 */
 	abstract public function getType(
-	);
+	):string;
 
 }
