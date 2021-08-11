11ddbeeeb267e5f6b47c5bbc234cb2623ec31f3c
plentymonkey <10546500+plentymonkey@users.noreply.github.com>
2021-08-11 12:04:09 +0200

    Automated interface update 2021-08-11_12:04:09

---
+++ Modules/Catalog/Services/UI/Options/Elements/Values/Elements/Base/Value.php
@@ -0,0 +1,43 @@
+<?php
+namespace Plenty\Modules\Catalog\Services\UI\Options\Elements\Values\Elements\Base;
+
+use Plenty\Modules\Catalog\Contracts\UI\UIValueContract;
+
+/**
+ * Base UI Option value
+ */
+abstract class Value 
+{
+
+	abstract public function toArray(
+	);
+
+	abstract public function getCaption(
+	):string;
+
+	abstract public function getValue(
+	);
+
+	abstract public function getColor(
+	):string;
+
+	abstract public function getIcon(
+	):string;
+
+	abstract public function setCaption(
+		string $caption
+	):Value;
+
+	abstract public function setValue(
+		 $value
+	):Value;
+
+	abstract public function setColor(
+		string $color
+	):Value;
+
+	abstract public function setIcon(
+		string $icon
+	):Value;
+
+}
