2be85caf05eaede8d4a89bc93e3efdf83bb162ba
plentymonkey <10546500+plentymonkey@users.noreply.github.com>
2021-09-30 12:05:52 +0200

    Automated interface update 2021-09-30_12:05:52

---
+++ Modules/Catalog/Services/UI/Filters/Elements/OperatorFilter.php
@@ -0,0 +1,106 @@
+<?php
+namespace Plenty\Modules\Catalog\Services\UI\Filters\Elements;
+
+use Plenty\Modules\Catalog\Contracts\UI\UIOptionContract;
+use Plenty\Modules\Catalog\Contracts\UI\UIOptionsContract;
+use Plenty\Modules\Catalog\Contracts\UI\UIValuesContract;
+use Plenty\Modules\Catalog\Services\UI\Filters\Elements\Base\BaseFilter;
+
+/**
+ * Filter of type: operator
+ */
+abstract class OperatorFilter 
+{
+
+	const TYPE_CHECKBOX = 'checkbox';
+
+	const TYPE_DATE = 'date';
+
+	const TYPE_MULTISELECT = 'multiselect';
+
+	const TYPE_NUMBER = 'number';
+
+	const TYPE_OPERATOR = 'operator';
+
+	const TYPE_SELECT = 'select';
+
+	const TYPE_TEXT = 'text';
+
+	const TYPE_TOGGLE = 'toggle';
+
+	const ALLOWED_TYPES = ['checkbox','date','multiselect','number','operator','select','text','toggle'];
+
+	/**
+	 * Accepts data, typically from the frontend
+	 */
+	abstract public function setData(
+		array $data
+	):self;
+
+	/**
+	 * Evaluates
+	 */
+	abstract public function evaluate(
+		array $settings = []
+	);
+
+	abstract public function toArray(
+	);
+
+	abstract public function getKey(
+	):string;
+
+	abstract public function getLabel(
+	):string;
+
+	abstract public function getType(
+	):string;
+
+	abstract public function getDefaultValue(
+	);
+
+	abstract public function isVisible(
+	):bool;
+
+	abstract public function isRequired(
+	):bool;
+
+	abstract public function getValues(
+	):UIValuesContract;
+
+	abstract public function getChildren(
+	):UIOptionsContract;
+
+	abstract public function setKey(
+		string $key
+	):BaseUIOption;
+
+	abstract public function setLabel(
+		string $label
+	):BaseUIOption;
+
+	abstract public function setType(
+		string $type
+	):BaseUIOption;
+
+	abstract public function setDefaultValue(
+		 $defaultValue
+	):BaseUIOption;
+
+	abstract public function setVisible(
+		bool $visible
+	):BaseUIOption;
+
+	abstract public function setRequired(
+		bool $required
+	):BaseUIOption;
+
+	abstract public function setValues(
+		UIValuesContract $values
+	):BaseUIOption;
+
+	abstract public function setChildren(
+		UIOptionsContract $children
+	):BaseUIOption;
+
+}
---
+++ Modules/Catalog/Services/UI/Filters/Filters.php
@@ -0,0 +1,52 @@
+<?php
+namespace Plenty\Modules\Catalog\Services\UI\Filters;
+
+use Illuminate\Support\Arr;
+use Plenty\Modules\Catalog\Contracts\Filters\CatalogFilterBuilderContract;
+use Plenty\Modules\Catalog\Contracts\UI\UIOptionContract;
+use Plenty\Modules\Catalog\Contracts\UI\UIOptionsContract;
+use Plenty\Modules\Catalog\Services\UI\Filters\Elements\DateFilter;
+use Plenty\Modules\Catalog\Services\UI\Filters\Elements\NumberFilter;
+use Plenty\Modules\Catalog\Services\UI\Filters\Elements\OperatorFilter;
+use Plenty\Modules\Catalog\Services\UI\Filters\Elements\SelectFilter;
+use Plenty\Modules\Catalog\Services\UI\Filters\Elements\TextFilter;
+use Plenty\Modules\Catalog\Services\UI\Options\UIOptions;
+use Plenty\Modules\ElasticSync\Models\Filter\Filter;
+use Version\Operator;
+
+/**
+ * Filters container
+ */
+abstract class Filters 
+{
+
+	abstract public function addLegacyFilterBuilder(
+		CatalogFilterBuilderContract $filterBuilder, 
+		array $data = []
+	);
+
+	abstract public function buildFiltersFromLegacyFilterBuilders(
+		array $settings = []
+	):Filters;
+
+	abstract public function getFilter(
+		string $key
+	);
+
+	abstract public function getDateFilters(
+	):array;
+
+	abstract public function getOperatorFilters(
+	):array;
+
+	abstract public function getDateFilter(
+		string $key
+	):DateFilter;
+
+	abstract public function add(
+	);
+
+	abstract public function toArray(
+	);
+
+}
