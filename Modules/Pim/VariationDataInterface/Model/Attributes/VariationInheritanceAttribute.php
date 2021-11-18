107417d25f16cbf78dd94a3bf62a7cbd145e7737
plentymonkey <10546500+plentymonkey@users.noreply.github.com>
2021-11-19 00:03:02 +0100

    Automated interface update 2021-11-19_00:03:02

---
+++ Modules/Pim/VariationDataInterface/Model/Attributes/VariationInheritanceAttribute.php
@@ -0,0 +1,31 @@
+<?php
+namespace Plenty\Modules\Pim\VariationDataInterface\Model\Attributes;
+
+use Plenty\Modules\Pim\VariationDataInterface\Contracts\AttributeInterface;
+use Plenty\Modules\Pim\VariationDataInterface\Model\VariationDataInterfaceContext;
+
+/**
+ * The market part of the variation
+ */
+abstract class VariationInheritanceAttribute 
+{
+
+	const PRIMARY_KEY = 'variationId';
+
+	/**
+	 * Get the related key on the variation model.
+	 */
+	abstract public static function getVariationAttribute(
+	):string;
+
+	abstract public function getName(
+	):string;
+
+	abstract public function addLazyLoadParts(
+		 $lazyLoadParts
+	);
+
+	abstract public function getLazyLoadParts(
+	);
+
+}
