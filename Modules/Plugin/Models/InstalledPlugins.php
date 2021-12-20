b98dfbd76706f7819634859d58087d8b65c92ce8
plentymonkey <10546500+plentymonkey@users.noreply.github.com>
2021-12-21 00:03:03 +0100

    Automated interface update 2021-12-21_00:03:03

--- Modules/Plugin/Models/InstalledPlugins.php
+++ Modules/Plugin/Models/InstalledPlugins.php
@@ -8,9 +8,9 @@ namespace Plenty\Modules\Plugin\Models;
 abstract class InstalledPlugins 
 {
 
-	const CREATED_AT = 'created_at';
+	const CREATED_AT = 'createdAt';
 
-	const UPDATED_AT = 'updated_at';
+	const UPDATED_AT = 'updatedAt';
 	
 public		$id;
 	
